680d88b3b1821900fcab5f34043e37914db65cad
EmmaJiangGH <jian1009@purdue.edu>
2024-10-27 05:48:25 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-
+import Background from "./Background.js";
 
 const WINDOW_WIDTH = 800;
 const WINDOW_HEIGHT = 200;
@@ -6,9 +6,9 @@ const SPRITE_WIDTH = 88 / 1.5;
 const SPRITE_HEIGHT = 94 / 1.5;
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
-const bgWidth = 600;
-const bgHeight = 234;
-const bgSpeed = 0.3;
+const BG_WIDTH = 600;
+const BG_HEIGHT = 234;
+const BG_SPEED = 0.3;
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 let sprite = null;
@@ -19,6 +19,11 @@ function createSprites(){
     const spriteHeightInGame = SPRITE_HEIGHT * scaleRatio;
     
     sprite = new SpriteImage(ctx, spriteWidthInGame, spriteHeightInGame, scaleRatio);
+
+    const bgWidthInGame = BG_WIDTH * scaleRatio;
+    const bgHeightInGame = BG_HEIGHT * scaleRatio;
+
+    bg = new Background(ctx, bgWidthInGame, bgHeightInGame, BG_SPEED, scaleRatio);
 }
 
 sprite.draw();
