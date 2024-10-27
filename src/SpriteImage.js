3a55b42c2e88f17f5852b00553c6e2e2c65a8174
Ashley Ko <ko198@purdue.edu>
2024-10-27 07:35:57 -0400

    Update SpriteImage.js

--- src/SpriteImage.js
+++ src/SpriteImage.js
@@ -1,6 +1,9 @@
 //draws sprite onto webpage
 export default class SpriteImage {
-    constructor(ctx, width, height, scaleRatio){
+    WALK_ANIMATION_TIMER = 200;
+    walkAnimationTimer = this.WALK_ANIMATION_TIMER;
+    spriteRunImages = [];
+    constructor(ctx,width, height, scaleRatio){
         this.ctx = ctx;
         this.canvas = ctx.canvas;
         this.width = width;
@@ -13,6 +16,32 @@ export default class SpriteImage {
         this.standingStillImage = new Image();
         this.standingStillImage.src = "images/standing_still.png";
         this.image = this.standingStillImage;
+
+        const spriteRunImage1 = new Image();
+        spriteRunImage1.src = "images/sprint_run1.png";
+
+        const sprintRunImage2 = new Image();
+        sprintRunImage2.src = "images/sprint_run2.png";
+
+        this.spriteRunImages.push(spriteRunImage1);
+        this.spriteRunImages.push(spriteRunImage2);
+    }
+
+    update (gameSpeed, frameTimeDelta){
+        this.run(gameSpeed, frameTimeDelta);
+    }
+
+    run(gameSpeed, frameTimeDelta){
+        if(this.walkAnimationTimer <= 0){
+            if(this.image = this.spriteRunImages[0]){
+                this.image = this.spriteRunImages[1]
+            }else{
+            this.image = this.spriteRunImages[0];
+        }
+            this.walkAnimationTimer = this.WALK_ANIMATION_TIMER;
+    }
+    this.walkAnimationTimer -= frameTimeDelta * gameSpeed;
+
     }
     draw(){
         this.ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
