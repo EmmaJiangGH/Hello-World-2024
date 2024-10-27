485700fc27b28e9fae646dde25d6c1d270987d8d
Ashley Ko <ko198@purdue.edu>
2024-10-27 05:20:06 -0400

    Update SpriteImage.js

--- src/SpriteImage.js
+++ src/SpriteImage.js
@@ -1,3 +1,21 @@
 //draws sprite onto webpage
+export default class Player {
+    constructor(ctx,width, height, scaleRatio){
+        this.ctx = ctx;
+        this.canvas = ctx.canvas;
+        this.width = width;
+        this.height = height;
+        this.scaleRatio = scaleRatio;
 
+        this.x = 10 * scaleRatio;
+        this.y = this.canvas.height - this.height - 1.5 * scaleRatio;
+
+        this.standingStillImage = new Image();
+        this.standingStillImage.src = "images/standing_still.png";
+        this.image = this.standingStillImage;
+    }
+    draw(){
+        this.ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+    }
+}
 //func loop thru animation
