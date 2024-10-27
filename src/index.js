449be29554633fcf69e51fd6ec8a5b0392c127c1
EmmaJiangGH <jian1009@purdue.edu>
2024-10-27 05:40:29 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ const WINDOW_WIDTH = 800;
 const WINDOW_HEIGHT = 200;
 const SPRITE_WIDTH = 88 / 1.5;
 const SPRITE_HEIGHT = 94 / 1.5;
+const canvas = document.getElementById('myCanvas');
+const ctx = canvas.getContext('2d');
+const bgWidth = 600;
+const bgHeight = 234;
+const bgSpeed = 0.3;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 let sprite = null;
 let scaleRatio = null;
 
