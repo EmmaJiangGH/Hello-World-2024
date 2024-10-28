2d238f369c9fd07e81ec6b2f6ccc2a787e66bae3
Kickblip <wyatt.zilker@gmail.com>
2024-10-28 12:28:07 -0500

    accidently closed pr

---
+++ public/Background.js
@@ -0,0 +1,34 @@
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+
+resizeCanvas();
+window.addEventListener("resize", resizeCanvas);
+
+const backgroundImg = new Image();
+backgroundImg.src = "images/bg.jpg";
+
+let backgroundX = 0;
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  ctx.drawImage(backgroundImg, backgroundX, 0, canvas.width, canvas.height);
+  ctx.drawImage(backgroundImg, backgroundX + canvas.width, 0, canvas.width, canvas.height);
+
+  backgroundX -= 2;
+
+  if (backgroundX <= -canvas.width) {
+      backgroundX = 0;
+  }
+
+  requestAnimationFrame(draw);
+}
+
+backgroundImg.onload = () => {
+  draw();
+};
